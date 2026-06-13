f8b9b040f9bbf96f3d8a9641fa26bc7b8295aa54
Dean Marano <here@dean.is>
2026-06-13 18:05:16 -0400

    refactor(activerecord): order new error classes to match Rails errors.rb

--- packages/activerecord/src/errors.ts
+++ packages/activerecord/src/errors.ts
@@ -33,18 +33,18 @@ export class AdapterNotSpecified extends ActiveRecordError {
   }
 }
 
-export class AdapterNotFound extends ActiveRecordError {
-  constructor(message?: string, options?: ErrorOptions) {
-    super(message, options);
-    this.name = "AdapterNotFound";
-  }
-}
-
 // Raised when a model makes a query but it has not specified an associated table.
 export class TableNotSpecified extends ActiveRecordError {
   constructor(message?: string, options?: ErrorOptions) {
     super(message, options);
     this.name = "TableNotSpecified";
+  }
+}
+
+export class AdapterNotFound extends ActiveRecordError {
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+    this.name = "AdapterNotFound";
   }
 }
 
@@ -271,6 +271,15 @@ export class TransactionRollbackError extends StatementInvalid {
   ) {
     super(message, options);
     this.name = "TransactionRollbackError";
+  }
+}
+
+// AsynchronousQueryInsideTransactionError will be raised when attempting
+// to perform an asynchronous query from inside a transaction.
+export class AsynchronousQueryInsideTransactionError extends ActiveRecordError {
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+    this.name = "AsynchronousQueryInsideTransactionError";
   }
 }
 
@@ -555,15 +564,6 @@ export class RangeError extends StatementInvalid {
   ) {
     super(message, options);
     this.name = "RangeError";
-  }
-}
-
-// AsynchronousQueryInsideTransactionError will be raised when attempting
-// to perform an asynchronous query from inside a transaction.
-export class AsynchronousQueryInsideTransactionError extends ActiveRecordError {
-  constructor(message?: string, options?: ErrorOptions) {
-    super(message, options);
-    this.name = "AsynchronousQueryInsideTransactionError";
   }
 }
 
