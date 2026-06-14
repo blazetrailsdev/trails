8a36898baff63a316d10e8736114e134426e8ef0
Dean Marano <here@dean.is>
2026-06-14 12:38:48 -0400

    feat(activesupport): implement BigDecimal#toString scientific ("E") notation

--- packages/activesupport/src/core-ext/bigdecimal.test.ts
+++ packages/activesupport/src/core-ext/bigdecimal.test.ts
@@ -8,4 +8,16 @@ describe("BigDecimalTest", () => {
     expect(bd.toString("+F")).toBe("+0.01");
     expect(bd.toString("+1F")).toBe("+0.0 1");
   });
+
+  it("to s with scientific notation", () => {
+    expect(new BigDecimal("1234.5678").toString("E")).toBe("0.12345678e4");
+    expect(new BigDecimal("1234.5678").toString("e")).toBe("0.12345678e4");
+    expect(new BigDecimal("1234.5678").toString("3E")).toBe("0.123 456 78e4");
+    expect(new BigDecimal("0.01").toString("E")).toBe("0.1e-1");
+    expect(new BigDecimal("100").toString("E")).toBe("0.1e3");
+    expect(new BigDecimal("120").toString("E")).toBe("0.12e3");
+    expect(new BigDecimal("0").toString("E")).toBe("0.0");
+    expect(new BigDecimal("-1234.5678").toString("E")).toBe("-0.12345678e4");
+    expect(new BigDecimal("1234.5678").toString("+E")).toBe("+0.12345678e4");
+  });
 });
