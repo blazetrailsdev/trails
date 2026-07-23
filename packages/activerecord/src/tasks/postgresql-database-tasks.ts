7862e5a192a102cfaaf5938f74e59b1bf2a16dda
Dean Marano <here@dean.is>
2026-07-22 21:32:50 -0400

    refactor(tasks): type tasks connection() as the concrete adapter

--- packages/activerecord/src/tasks/postgresql-database-tasks.ts
+++ packages/activerecord/src/tasks/postgresql-database-tasks.ts
@@ -12,6 +12,7 @@ import {
   type SpawnSyncResult,
 } from "@blazetrails/activesupport";
 import type { AbstractAdapter as DatabaseAdapter } from "../connection-adapters/abstract-adapter.js";
+import type { PostgreSQLAdapter } from "../connection-adapters/postgresql-adapter.js";
 import type { DatabaseConfig } from "../database-configurations/database-config.js";
 import { DatabaseAlreadyExists } from "../errors.js";
 import { Base } from "../base.js";
@@ -125,10 +126,7 @@ export class PostgreSQLDatabaseTasks {
   }
 
   async collation(): Promise<string> {
-    const conn = (await this.connection()) as DatabaseAdapter & {
-      collation(): Promise<string>;
-    };
-    return conn.collation();
+    return (await this.connection()).collation();
   }
 
   async purge(): Promise<void> {
@@ -275,8 +273,8 @@ export class PostgreSQLDatabaseTasks {
     return String(this.configurationHash.encoding ?? defaultEncoding());
   }
 
-  private async connection(): Promise<DatabaseAdapter> {
-    return Base.connectionPool().leaseConnection();
+  private async connection(): Promise<PostgreSQLAdapter> {
+    return (await Base.connectionPool().leaseConnection()) as PostgreSQLAdapter;
   }
 
   private async closeAdapter(adapter: DatabaseAdapter): Promise<void> {
