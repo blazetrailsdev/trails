aca02fef33467c8968ead9df4b6ec9cc13661330
Dean Marano <here@dean.is>
2026-09-02 14:02:12 -0400

    fix(activemodel): keep the slot's @internal blocks flush against their declarations

--- packages/activemodel/src/attribute/user-provided-default-slot.ts
+++ packages/activemodel/src/attribute/user-provided-default-slot.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 /** @internal */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export let _UserProvidedDefaultCtor: (new (...args: any[]) => any) | undefined;
 
 /** @internal */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function _setUserProvidedDefaultCtor(ctor: new (...args: any[]) => any): void {
   _UserProvidedDefaultCtor = ctor;
 }
