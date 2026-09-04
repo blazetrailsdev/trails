1d1c659734d6207ce72e1befa7f9d5ca7da65650
Dean Marano <here@dean.is>
2026-09-04 16:09:46 -0400

    test(trailties): move hooks test to the Rails mirror path

--- packages/trailties/src/application/initializers/hooks.test.ts
+++ packages/trailties/src/application/initializers/hooks.test.ts
@@ -1,7 +1,7 @@
 import { afterEach, describe, expect, it } from "vitest";
 import { resetLoadHooks } from "@blazetrails/activesupport";
-import { Application } from "../application.js";
-import { Trails } from "../rails.js";
+import { Application } from "../../application.js";
+import { Trails } from "../../rails.js";
 
 describe("HooksTest", () => {
   afterEach(() => {
