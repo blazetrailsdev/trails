c785bffe9914f1d280e267a046035fc44012b7ac
Dean Marano <here@dean.is>
2026-09-15 11:05:02 -0400

    test(activemodel): EmailValidator message follows Ruby || truthiness

--- packages/activemodel/src/test-helpers/validators/email-validator.ts
+++ packages/activemodel/src/test-helpers/validators/email-validator.ts
@@ -5,8 +5,9 @@ import type { ValidatableRecord } from "../../validator.js";
 export class EmailValidator extends EachValidator {
   validateEach(record: ValidatableRecord, attribute: string, value: unknown): void {
     if (!/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(String(value ?? ""))) {
+      const message = this.options.message as string | false | null | undefined;
       record.errors.add(attribute, undefined, {
-        message: (this.options.message as string | undefined) ?? "is not an email",
+        message: message != null && message !== false ? message : "is not an email",
       });
     }
   }
