31879a1235a8c85816654424cb910beebbbe6e25
Dean Marano <here@dean.is>
2026-09-16 12:57:59 -0400

    test(activesupport): restore Time.zone after duration no-zone test

--- packages/activesupport/src/core-ext/duration.test.ts
+++ packages/activesupport/src/core-ext/duration.test.ts
@@ -5,7 +5,7 @@ import { rbEqual, rbInspect as inspect } from "@blazetrails/ruby-compat";
 import { assertNothingRaised, assertRaise, assertRaises } from "../testing/assertions.js";
 import { TimeWithZone } from "../time-with-zone.js";
 import { TimeZone } from "../values/time-zone.js";
-import { setZone } from "../time-zone-config.js";
+import { setZone, zone } from "../time-zone-config.js";
 import { plusWithDuration as timePlusWithDuration } from "./time/calculations.js";
 import { ArgumentError } from "../hash-utils.js";
 import { current, minusWithDuration, plusWithDuration } from "./date/calculations.js";
@@ -342,6 +342,7 @@ describe("DurationTest", () => {
   });
 
   it("since and ago anchored to time now when time zone is not set", () => {
+    const oldZone = zone();
     setZone(null);
     vi.useFakeTimers();
     try {
@@ -356,6 +357,7 @@ describe("DurationTest", () => {
       );
     } finally {
       vi.useRealTimers();
+      setZone(oldZone);
     }
   });
 
