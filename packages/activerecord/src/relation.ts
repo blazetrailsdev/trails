429e826425c1251860bfeb794b372d88f164c2ff
Dean Marano <here@dean.is>
2026-09-23 16:28:34 -0400

    refactor(activerecord): retire interface receipts that cover no extra surface

--- packages/activerecord/src/relation.ts
+++ packages/activerecord/src/relation.ts
@@ -1900,7 +1900,6 @@ export class Relation<T extends Base> {
 _registerRelationFamily("relation", Relation);
 
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-/** @noRailsEquivalent PERMANENT */
 export interface RelationScopes<T extends Base> {}
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
