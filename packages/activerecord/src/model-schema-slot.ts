3c2a0dd7cffd659714b71d8b5cc18cb19b9315bd
Dean Marano <here@dean.is>
2026-09-22 21:31:00 -0400

    refactor(activerecord): delete orphaned model-schema-slot

--- packages/activerecord/src/model-schema-slot.ts
+++
@@ -1,9 +0,0 @@
-/** @internal */
-export let _deriveJoinTableName: ((firstTable: string, secondTable: string) => string) | undefined;
-
-/** @internal */
-export function _setDeriveJoinTableName(
-  fn: (firstTable: string, secondTable: string) => string,
-): void {
-  _deriveJoinTableName = fn;
-}
